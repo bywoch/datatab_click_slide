f9e0ad6db63ead7ad214930f648c503bef4ce1f5
Bright Chung <bywoch@gmail.com>
2021-02-16 11:00:00 +0900

    update files

--- js/common.js
+++ js/common.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c81318d (update js)
-=======
->>>>>>> 3d49372 (update js)
     /*-- ※ guide_map Area Strat ※ --*/
     $('.gbt-01').click(function () {
         $('.guide_map .gm_wrap .gm_btn_bx ul .gbt-01').css('background', 'url(images/nvz_on-001.png) 0 0 no-repeat');
@@ -31,33 +23,11 @@ $(document).ready(function () {
 
         $(this).addClass('current');
         $("#" + tab_id).addClass('current');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 811f7ac (update js)
-=======
->>>>>>> 1d48ade (update js)
         $('.gm_slider').css({
             'position': 'relative',
             'transition-duration': '0.2s',
             'transform': 'translate3d(-660px, 0px, 0px)'
         });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-        $('.gm_slider li:eq(1)').attr('aria-hidden', 'false');
->>>>>>> c81318d (update js)
-=======
->>>>>>> 811f7ac (update js)
-=======
-
-        $('.gm_slider li:eq(1)').attr('aria-hidden', 'false');
->>>>>>> 3d49372 (update js)
-=======
->>>>>>> 1d48ade (update js)
     });
     $('.gs_01').bxSlider({
         auto: false,
@@ -71,31 +41,7 @@ $(document).ready(function () {
         controls: true,
         hideControlOnEnd: true,
         moveSlides: 1,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         pagerCustom: '#gm_slt_02'
-=======
-=======
->>>>>>> 3d49372 (update js)
-        pagerCustom: '#gm_slt_02',
-        onSlideAfter: function($slideElement, oldIndex, newIndex) {
-        // newIndex 값이 1인 경우 (두 번째 li 요소) aria-hidden 속성 값을 "false"로 설정
-        if (newIndex === 1) {
-            $slideElement.next().attr('aria-hidden', 'false');
-        }
-    }
-<<<<<<< HEAD
->>>>>>> c81318d (update js)
-=======
-        pagerCustom: '#gm_slt_02'
->>>>>>> 811f7ac (update js)
-=======
->>>>>>> 3d49372 (update js)
-=======
-        pagerCustom: '#gm_slt_02'
->>>>>>> 1d48ade (update js)
         
     });
     $('.gs_03').bxSlider({
@@ -103,37 +49,7 @@ $(document).ready(function () {
         controls: true,
         hideControlOnEnd: true,
         moveSlides: 1,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         pagerCustom: '#gm_slt_03'
     });
-=======
->>>>>>> 7353dfa (upload files)
-=======
-=======
->>>>>>> 3d49372 (update js)
-        pagerCustom: '#gm_slt_03',
-        onSlideAfter: function($slideElement, oldIndex, newIndex) {
-        // newIndex 값이 1인 경우 (두 번째 li 요소) aria-hidden 속성 값을 "false"로 설정
-        if (newIndex === 1) {
-            $slideElement.next().attr('aria-hidden', 'false');
-        }
-    }
-<<<<<<< HEAD
-=======
-        pagerCustom: '#gm_slt_03'
->>>>>>> 811f7ac (update js)
-=======
-        pagerCustom: '#gm_slt_03'
->>>>>>> 1d48ade (update js)
-    });
->>>>>>> c81318d (update js)
-=======
->>>>>>> ecb1f87 (upload files)
-=======
-    });
->>>>>>> 3d49372 (update js)
 
 });
